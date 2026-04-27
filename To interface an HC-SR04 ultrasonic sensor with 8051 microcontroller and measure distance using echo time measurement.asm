db28db7cb3be2b61227f9b265107da8115a9475a
Piyush Agrawal <piyusha871@gmail.com>
2026-04-27 07:47:45 +0530

    Update To interface an HC-SR04 ultrasonic sensor with 8051 microcontroller and measure distance using echo time measurement.asm

--- To interface an HC-SR04 ultrasonic sensor with 8051 microcontroller and measure distance using echo time measurement.asm
+++ To interface an HC-SR04 ultrasonic sensor with 8051 microcontroller and measure distance using echo time measurement.asm
@@ -1,22 +1,51 @@
-MOV TMOD, #01H ; Timer0 Mode1
-CLR TR0
-TRIGGER:
-SETB P2.0 ; Trigger HIGH
-ACALL DELAY10
-CLR P2.0 ; Trigger LOW
-WAIT_ECHO:
-JNB P2.1, WAIT_ECHO ; Wait for ECHO HIGH
-SETB TR0 ; Start Timer
-STOP:
-JB P2.1, STOP ; Wait until ECHO goes LOW
-CLR TR0 ; Stop timer
-MOV A, TL0 ; Read timer value
-MOV B, #58
-DIV AB ; Distance = Time/58 approx
+ORG 0000H
 
-SJMP TRIGGER
+MOV TMOD, #01H        ; Timer0 Mode1 (16-bit)
+
+MAIN:
+
+; -------- Trigger Pulse --------
+SETB P2.0             ; Trigger HIGH
+ACALL DELAY10
+CLR P2.0              ; Trigger LOW
+
+; -------- Wait for Echo HIGH --------
+WAIT_ECHO:
+    JNB P2.1, WAIT_ECHO
+
+; -------- Reset Timer --------
+CLR TR0
+MOV TH0, #00H
+MOV TL0, #00H
+
+; -------- Start Timer --------
+SETB TR0
+
+; -------- Wait for Echo LOW --------
+WAIT_LOW:
+    JB P2.1, WAIT_LOW
+
+; -------- Stop Timer --------
+CLR TR0
+
+; -------- Read Timer Value (16-bit) --------
+MOV A, TL0            ; Low byte
+MOV B, TH0            ; High byte
+
+; (Simple approximation using only TL0)
+; For lab/demo purpose
+MOV A, TL0
+MOV B, #58
+DIV AB                ; Distance ≈ Time / 58
+
+; A now contains distance in cm
+
+SJMP MAIN             ; Repeat continuously
+
+; -------- Delay Subroutine (~10us) --------
 DELAY10:
-MOV R7, #30 ; ~10 us delay
+    MOV R7, #30
 D1: DJNZ R7, D1
 RET
+
 END
